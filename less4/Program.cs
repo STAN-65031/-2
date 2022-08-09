859d51bf7949ec132b62033bdffe1d23dcce839c
STAN-65031 <lox.ibany@gmail.com>
2022-08-09 22:10:00 +0300

    2 прогграмы в одной

---
+++ less4/Program.cs
@@ -0,0 +1,38 @@
+﻿
+void definesMultiple(){
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (((number % 7) == 0) & ((number % 23) == 0)){
+    Console.WriteLine ("да");
+}
+else {
+    Console.WriteLine ("нет");
+}
+}
+
+definesMultiple();
+definesMultiple();
+
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// int number2 = Convert.ToInt32(Console.ReadLine()); 
+
+
+// void determinesSuare(int numberA, int numberB){
+
+// if (numberA == numberB){
+//     Console.WriteLine("числа равны. Введите другие числа");
+// }
+// int numberSquare = numberA * numberA;
+
+// if (numberSquare == numberB){
+//      Console.WriteLine ("да");
+// }
+// else {
+//    Console.WriteLine ("нет");
+// }
+// }
+
+// determinesSuare(number1, number2);
+// determinesSuare(number1, number2);
