b76dbb67e6bed187a82a502cf35ff634c85886cb
STAN-65031 <lox.ibany@gmail.com>
2022-08-09 21:42:21 +0300

    кратные числа

---
+++ less3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+void findMultiple(){
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if ((number1 % number2) == 0 ){
+    Console.WriteLine ("да");
+}
+else {
+     int newNumber = number1 % number2;
+     Console.WriteLine (newNumber);
+    Console.WriteLine ("нет");
+}
+}
+
+findMultiple();
+findMultiple();
