721188979258fd8f861625ff8ef6da50e7a192c5
STAN-65031 <lox.ibany@gmail.com>
2022-08-09 21:13:05 +0300

    сщздал новый метод

---
+++ less2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+void newmetod(){
+int randomValue = new Random().Next(100, 1000);
+
+Console.WriteLine(randomValue);
+
+int number1 = randomValue / 100;
+int number3 = randomValue % 10;
+
+Console.WriteLine(number1);
+Console.WriteLine("первое число");
+Console.WriteLine(number3);
+Console.WriteLine("третье число");
+
+int newNumber = number1 * 10 + number3;
+Console.WriteLine(newNumber);
+}
+
+newmetod();
+newmetod();
