57daa9d192f318aac62275e02f9e93b29d9acf38
STAN-65031 <lox.ibany@gmail.com>
2022-08-09 20:48:02 +0300

    рандомное число и большее из числа

---
+++ less1/Program.cs
@@ -0,0 +1,25 @@
+﻿//int number = Convert.ToInt32(Console.ReadLine());
+
+Random random = new Random();
+int randomValue = random.Next(10,100);
+
+Console.WriteLine(randomValue);
+
+int lastnumber = randomValue % 10;
+int firstnumber = randomValue / 10;
+
+int max = lastnumber;
+
+ if (lastnumber == firstnumber){
+    Console.WriteLine("числа равны");
+ }
+ else{
+    if (max < firstnumber){
+        max = firstnumber;
+    }
+    Console.WriteLine(max);
+    Console.WriteLine(" МАКСИМАЛЬНОЕ");
+ }
+
+Console.WriteLine(lastnumber);
+Console.WriteLine(firstnumber);
