ee0d9aea44bb814ff680b6c412dae523f777d919
STAN-65031 <lox.ibany@gmail.com>
2022-08-10 00:13:52 +0300

    домашка 1

---
+++ homeWork1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+void findsnumber (int number2){
+
+    int numberSekond = (number2 % 100) / 10; 
+    Console.WriteLine(numberSekond);
+}
+
+findsnumber(number);
