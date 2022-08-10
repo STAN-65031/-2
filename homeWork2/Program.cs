94d2671b222b028c3648ecc2cbe0834d51374385
STAN-65031 <lox.ibany@gmail.com>
2022-08-11 00:25:23 +0300

    сделал дз2

---
+++ homeWork2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+void findsnumber (int number3){
+
+    string str = number3.ToString();
+    if (number3 > 100)
+    {
+      Console.WriteLine(str[2]);
+    }
+    else{
+    Console.WriteLine("третьей цифры нет");
+}
+}
+
+findsnumber(number);
